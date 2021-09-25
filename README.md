317429aafceb50448e17e176fbc567de40220f2c
Al Hera Ahmad <alheraahmad0786@gmail.com>
2021-09-25 09:17:29 +0530

    styles and links added

---
+++ README.md
@@ -0,0 +1,8 @@
+# Lucky Birthday WebApp
+This WebApp takes your birthday in dd-mm-yyy format and takes your lucky number is really a lucky number or not. Built in VanillaJS, HTML & CSS.
+
+---
+## Technologies used
+- HTML
+- CSS
+- JavaScript
--- app.js
+++ app.js
@@ -6,17 +6,13 @@ const result = document.querySelector("#result");
 var sum = 0 ;
 function checkButtonHandler(){
     var numArr = [] ;
-    console.log(numArr) ;
-    console.log(dateOfBirth.value);
     const dob = dateOfBirth.value ;
     var numDOB= dob.replaceAll('-','0');
     var numDOBValue = Number(numDOB) ;
-    console.log(numDOB) ;
     for(let i=9 ; i>-1 ; i--){
         numArr[i] = Math.trunc(numDOBValue / Math.pow(10,i));
         numDOBValue = numDOBValue - (numArr[i]*(Math.pow(10,i))) ;
     }
-    console.log(numArr);
     for(let j=0 ; j<10 ;j++){
         sum = numArr[j] + sum ;
     }
